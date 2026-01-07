77b43e8ee8e16575d3bb17b7a414bb944ce05d50
Anus-Ali-Khan <anusalikhan1405@gmail.com>
2026-01-07 23:43:52 +0500

    [WIP]: agent creation

---
+++ python_code/api/agents/utils.py
@@ -0,0 +1,43 @@
+from google.genai import types
+from sentence_transformers import SentenceTransformer
+
+#  Load model (downloads automatically on first run)
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+
+# Give chatbot response
+def get_chatbot_response(client, model_name, messages):
+    input_messages = []
+    for message in messages:
+        input_messages.append(types.Content(role=message['role'], parts=[types.Part.from_text(text=message['content'])]))
+    response = client.models.generate_content(
+        model=model_name,
+        contents=input_messages,
+    #     config = types.GenerateContentConfig(
+    #     response_mime_type='application/json',
+    #     response_schema=responseFormatType,
+    # ),
+    ).candidates[0].content.parts[0].text
+    finalCleanedResponse = cleaned_response(response)
+    return finalCleanedResponse
+
+# Clean the output received from gemini
+def cleaned_response(response):
+    cleaned = response.strip()
+
+    if cleaned.startswith("```json"):
+        cleaned = cleaned.removeprefix("```json").strip()
+
+    if cleaned.endswith("```"):
+        cleaned = cleaned.removesuffix("```").strip()
+    return cleaned 
+
+# Generate embeddings
+def get_embeddings(text_input):
+    output = model.encode(text_input)
+
+    embeddings = []
+    for embeddings_object in output:
+        embeddings.append(embeddings_object)
+
+    return embeddings
---
+++ python_code/api/development_code.py
@@ -0,0 +1,36 @@
+from agents import GuardAgent,ClassificationAgent
+import os
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    guard_agent = GuardAgent()
+    classification_agent = ClassificationAgent()
+
+    messages = []
+    while True:
+        # os.system('cls' if os.name == 'nt' else 'clear')
+
+        print("\n\n Print Messages ........")
+        for message in messages:
+            print(f"{message['role']}: {message['content']}")
+
+        # Get user input
+        prompt = input("User: ")
+        messages.append({"role":"user","content":prompt})
+
+        # Get Guard Agent's response
+        guard_agent_response = guard_agent.get_response(messages)
+        
+        if guard_agent_response["memory"]["guard_decision"] == "not allowed":
+            messages.append(guard_agent_response)
+            continue
+
+        # Get Classification Agent's Response
+        classification_agent_response = classification_agent.get_response(messages)
+        chosen_agent = classification_agent_response["memory"]["classification_decision"]
+        print("Chosen Agent: ", chosen_agent)
+         
+
